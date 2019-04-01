2cb31640d79aa3da160dd3cfe9d64082578058a9
geekdaniels <oladotun.olufemi@yahoo.com>
2019-04-01 15:29:08 +0100

    Expense Update

--- js/main.js
+++ js/main.js
@@ -165,9 +165,10 @@ $(function () {
  if ($('#formValidate').length) {
   $('#formValidate').validator();
  }
- if ($('#dataTable1').length) {
-  $('#dataTable1').DataTable({ buttons: ['copy', 'excel', 'pdf'] });
- }
+ 
+// if ($('#dataTable1').length) {
+//  var table  = $('#dataTable1').DataTable({ buttons: ['copy', 'excel', 'pdf'] });
+// }
  // #6. EDITABLE TABLES
 
  if ($('#editableTable').length) {
